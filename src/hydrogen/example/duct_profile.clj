f73fbb5d16acee69d4ea066d867602a207dd696b
Damian Hryniewicz <damian@magnet.coop>
2019-01-18 10:50:48 +0100

    first commit

---
+++ src/hydrogen/example/duct_profile.clj
@@ -0,0 +1,13 @@
+(ns hydrogen.example.duct-profile
+    (:require [leiningen.new.templates :refer [renderer]]))
+
+(def render (renderer "hydrogen.example"))
+
+(defn hydrogen.example [data]
+      (prn)
+      (prn "HELLO!")
+      (prn)
+      {:extra-deps ['[magnetcoop/stork "0.1.5"]
+                    '[aramis "0.1.1"]]
+       :extra-files [["resources/{{dirs}}/parrot.txt" (render "parrot.txt" data)]
+                     ["src/{{dirs}}/take_over_the_world.clj" (render "take_over_the_world.clj" data)]]})

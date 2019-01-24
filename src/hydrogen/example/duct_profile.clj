24221a9298ea7015e61394d6a112ccd378b82cc6
Damian Hryniewicz <damian@magnet.coop>
2019-01-24 19:39:20 +0100

    use profile-files and profile-data

--- src/hydrogen/example/duct_profile.clj
+++ src/hydrogen/example/duct_profile.clj
@@ -3,8 +3,12 @@
 
 (def render (renderer "hydrogen.example"))
 
-(defn hydrogen.example [data]
-      {:extra-deps ['[magnetcoop/stork "0.1.5"]
-                    '[aramis "0.1.1"]]
-       :extra-files [["resources/{{dirs}}/parrot.txt" (render "parrot.txt" data)]
-                     ["src/{{dirs}}/take_over_the_world.clj" (render "take_over_the_world.clj" data)]]})
+(defn profile-data [project-name]
+      {:thats :amazing
+       :deps [['magnetcoop/stork "0.1.5"]
+              ['aramis "0.1.1"]]})
+
+(defn profile-files [data]
+      [["resources/{{dirs}}/parrot.txt" (render "parrot.txt" data)]
+       ["src/{{dirs}}/take_over_the_world.clj" (render "take_over_the_world.clj" data)]
+       "resources/{{dirs}}/new-directory"])
